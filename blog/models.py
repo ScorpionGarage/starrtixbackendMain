30e1dd849c264d163ff21ec494bb5b1763b1ce58
Judah40 <judahdore@gmail.com>
2024-04-04 12:46:37 +0000

    django downgrade

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,8 @@ class blog(models.Model):
     description = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
+    image=models.ImageField(upload_to='images/' ,default="freepik.com")
+    video = models.FileField(upload_to='videos/', default="freepik.com")
     
     def __str__(self):
         return self.title
