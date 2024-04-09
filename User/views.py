dafb8485f14d7ff39754d9b215f860aa7451eac3
Judah40 <judahdore@gmail.com>
2024-04-09 10:48:59 +0000

    update

--- User/views.py
+++ User/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status,generics,viewsets,permissions
 from .serializer import RegisterSerializer
+from rest_framework.decorators import action
 
 User = get_user_model()
 class RegisterView(APIView):
@@ -51,3 +52,26 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.IsAdminUser] 
+    
+    @action(detail=False, method=['post'])
+    def custom_create(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if not queryset.exists():
+            # No data found, returning 401 Unauthorized
+            return Response({"detail": "No data found."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        page = self.paginate_queryset(queryset)
+        if page is not limited:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        serializer= self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user=serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
